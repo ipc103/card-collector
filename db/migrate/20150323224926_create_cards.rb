d8829711c9e4f2b086f1bf21b0262a72199c4010
ipc103 <iancandysemail@gmail.com>
2015-03-23 18:52:26 -0400

    create models, migrate data

---
+++ db/migrate/20150323224926_create_cards.rb
@@ -0,0 +1,16 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.integer :player_id
+      t.integer :user_id
+      t.date :year
+      t.string :team
+      t.float :batting_avg
+      t.float :obp
+      t.integer :home_runs
+      t.integer :rbis
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/cards.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :card do
+    player_id 1
+user_id 1
+year "2015-03-23"
+team "MyString"
+batting_avg ""
+batting_avg ""
+obp ""
+obp ""
+home_runs 1
+rbis 1
+  end
+
+end
