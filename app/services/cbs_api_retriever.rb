574e2522f811cca6721f2d8a90d70ecb3cfd5873
ipc103 <iancandysemail@gmail.com>
2015-04-02 17:34:59 -0400

    update views

--- app/services/cbs_api_retriever.rb
+++ app/services/cbs_api_retriever.rb
@@ -1,12 +1,6 @@
 class CBS_API_Retriever
   URL = "http://api.cbssports.com/fantasy/players/list?version=3.0&response_format=json&SPORT=baseball&callback=?"
 
-  attr_reader :response
-
-  def initialize
-    # @response ||= get_api_data
-  end
-
   def get_api_data
     HTTParty.get(URL)
   end
