14c240aaced15a00d725a8c8cc129b05fa00de69
ipc103 <iancandysemail@gmail.com>
2015-04-03 18:09:42 -0400

    add giphy interface

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -5,8 +5,17 @@ $(function(){
 })
 
 $(function(){
-  $('.player-card').on("mouseenter", function(){
-      debugger;
-     $(this).parent.width($(this).parent.width() + 30);  })
+  $('.player-card').mouseover(function(){
+    var $card = $(this)
+    var name = $(this).find("h4").html().split(" ");
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + name[0] + "+" + name[1] + "&api_key=dc6zaTOxFJmzC";
+    $.getJSON(url, function(data){
+      var num = Math.floor(Math.random() * data["data"].length)
+      var img = data["data"][num]["images"]["fixed_height_small"]["url"]
+      $card.find("img").attr("src", img)
+    });
+  })
+    
+  //   // 
   
 })
