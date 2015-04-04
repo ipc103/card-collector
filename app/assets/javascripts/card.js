744c5e55708d2c099486588ffb45693760e4a310
ipc103 <iancandysemail@gmail.com>
2015-04-04 13:11:36 -0400

    update img click listener, add styling to show page

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -5,17 +5,15 @@ $(function(){
 })
 
 $(function(){
-  $('.player-card').mouseover(function(){
-    var $card = $(this)
-    var name = $(this).find("h4").html().split(" ");
+  $('.player-card img').click(function(e){
+    e.stopPropagation();
+    var $img = $(this)
+    var name = $(this).parent().find("h4").html().split(" ");
     var url = "http://api.giphy.com/v1/gifs/search?q=" + name[0] + "+" + name[1] + "&api_key=dc6zaTOxFJmzC";
     $.getJSON(url, function(data){
       var num = Math.floor(Math.random() * data["data"].length)
       var img = data["data"][num]["images"]["fixed_height_small"]["url"]
-      $card.find("img").attr("src", img)
+      $img.attr("src", img)
     });
-  })
-    
-  //   // 
-  
+  })  
 })
