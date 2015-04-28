f4bb15ac1d1d34eece95b176893ac47dd6ea08e0
ipc103 <iancandysemail@gmail.com>
2015-04-28 12:28:45 -0400

    add data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require_relative "player_data.rb"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,12 +7,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 api = CBS_API_Retriever.new
-players = [{"firstname"=>"Andrew","photo"=>"https://www.cbssports.com/images/baseball/mlb/players/170x170/1103290.png","position"=>"CF","icons"=>{"video"=>1,"headline"=>"Pirates' McCutchen has no worries about being ready for start of season"},"lastname"=>"McCutchen","age"=>28,"pro_status"=>"A","jersey"=>"22","fullname"=>"Andrew McCutchen","id"=>"1103290","pro_team"=>"PIT"}]
 
-
-players.each do |player|
+PlayerData.new.players.each do |player|
   p = Player.new
   p.name = player["fullname"]
   p.photo = player["photo"]
